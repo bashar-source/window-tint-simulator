a782a77a396ab763cc354a54aea55ef474eff489
bashar-source <bashar@69toolz.com>
2025-08-25 17:41:29 -0700

    Minimal React app: add src/App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Window Tint Simulator</h1>
+      <p>Welcome! Your React app is running.</p>
+    </div>
+  );
+}
+
+export default App;
